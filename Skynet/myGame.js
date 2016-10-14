3e8624bfbed6458495020e6de6eb71352f96748b
Akidd <akidd.de@mysummitps.org>
2016-10-14 17:34:06 +0000

    first commit

--- Skynet/myGame.js
+++ Skynet/myGame.js
@@ -7,6 +7,8 @@ var hitPlatform;
 var player;
 var cursors;
 var stars;
+var i;
+var update
 function preload() {
 game.load.image('sky', 'assets/sky.png');
 game.load.image('ground', 'assets/platform.png');
@@ -16,34 +18,34 @@ game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 }
 
 function create() {
-// We're going to be using physics, so enable the Arcade Physics system
+
 game.physics.startSystem(Phaser.Physics.ARCADE);
 game.add.sprite(0, 0, 'sky');
 game.add.sprite(0, 0, 'star');
-// The platforms group contains the ground and the 2 ledges we can jump on
+
 platforms = game.add.group();
-// We will enable physics for any object that is created in this group
+
 platforms.enableBody = true;
-// Here we create the ground
+
 var ground = platforms.create(0, game.world.height - 64, 'ground');
-// Scale it to fit the width of the game (the original sprite is 400x32 in size)
+
 ground.scale.setTo(2, 2);
-// This stops it from falling away when you jump on it
+
 ground.body.immovable = true;
 ledge = platforms.create(420, 420, 'ground');
 ledge = platforms.create(35, 150, 'ground');
 ledge.body.immovable = true;
-// The player and its settingsasddddw
+
 player = game.add.sprite(320, game.world.height - 150, 'dude');
-//We need to enable physiccs on the player
+
 game.physics.arcade.enable(player);
 
-// Player physics properties. Give the little guy a slight bounce.
+
 player.body.bounce.y = 0.2;
 player.body.gravity.y = 200;
 player.body.collideWorldBounds = true;
 
-// Our two animations, walking left and right.
+
 
 player.animations.add('left', [0, 1, 2, 3], 10, true);
 player.animations.add('right', [5, 6, 7, 8], 10, true);
@@ -52,31 +54,44 @@ cursors = game.input.keyboard.createCursorKeys();
 
 
 stars = game.add.group();
+
 stars.enableBody = true;
+
+
+for (i = 0; i < 12; i++)
+{
+
+var star = stars.create(i * 70, 0, 'star');
+
+
+star.body.gravity.y = 6;
+
+
+star.body.bounce.y = 0.7 + Math.random() * 0.2;
 }
 
 function update() {
 
-// Collide the player and the stars with the platforms
+
 
 hitPlatform = game.physics.arcade.collide(player, platforms);
 
-// Reset the player velocity (movement)
+
 player.body.velocity. x = 0;
 
 if (cursors.left.isDown) 
 {
     
-// Move to the left 
+
 
 player.body.velocity.x = -150;
 player.animations.play('left');
 
+
 }
+else if (cursors.right.isDown)
+{
 
-if (cursors.right.isDown){
-
-// Move to the right
 player.body.velocity.x = 150;
 player.animations.play('right');
 
@@ -84,15 +99,17 @@ player.animations.play('right');
 else
 {
 
-// Stand still
+
 player.animations.stop();
 player.frame = 4;
 
     
 }
 
-// Allow the player to jump if they are touching the ground
+
 if (cursors.up.isDown && player.body.touching.down && hitPlatform)
+
 player.body.velocity.y = -350;
 
+}
 }
